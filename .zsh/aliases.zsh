2f08cfe9f685fb1b9f9cc9cc4099f839c069a09c
Saij <mail+github@ch-friedrich.net>
2024-03-18 21:12:30 +0100

    Second commit

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,7 +4,11 @@
 alias k="kubectl"
 alias h="helm"
 
-alias code="open -a 'Visual Studio Code'"
+CODE_CMD=$(which code)
+OPEN_CMD=$(which open)
+if [ -z ${CODE_CMD+x} ] && [ ! -z ${OPEN_CMD+x} ]; then
+	alias code="open -a 'Visual Studio Code'"
+fi
 
 alias ls="eza --icons --group-directories-first"
 alias ll="eza --icons --group-directories-first -l"
