ff4c41dba1e41a5b2e9a1a35e4b71a53836405a0
Saij <mail+github@ch-friedrich.net>
2024-03-18 20:58:40 +0100

    Inital commit

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,11 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+
+alias code="open -a 'Visual Studio Code'"
+
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
+alias grep='grep --color'
---
+++ .zshenv
@@ -0,0 +1,7 @@
+# Added locations to path variable
+#export PATH=$PATH:$HOME/.local/bin
+
+export EDITOR=nano
+export KUBE_EDITOR=nano
+
+
