4be150ab76d702349aa314f66a429bbe642e4ea9
Christoph Friedrich <github@ch-friedrich.net>
2024-04-10 07:11:13 +0200

    Fixed NVM bug

--- packages/zsh/zsh/nvm.zsh
+++ packages/zsh/zsh/nvm.zsh
@@ -1,5 +1,6 @@
 # NVM lazy load
 if [ -s "$HOME/.nvm/nvm.sh" ]; then
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
     alias nvm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && nvm'
     alias node='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && node'
